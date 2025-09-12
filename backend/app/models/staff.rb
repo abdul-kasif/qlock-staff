7e560855124060de280386106bb5370bd8e24131
abdul-kasif <abdulkasif.ra@gmail.com>
2025-09-12 22:33:33 +0530

    feat(auth-api): add email validation logic for Staff model

--- backend/app/models/staff.rb
+++ backend/app/models/staff.rb
@@ -1,2 +1,12 @@
 class Staff < ApplicationRecord
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, if: -> { email.present? }
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase if email.present?
+  end
 end
