2d1b472f34249154af3338d48705c26ea48752b9
abdul-kasif <abdulkasif.ra@gmail.com>
2025-09-13 12:59:41 +0530

    feat(auth-api): create SendOtpMailer job and configure it with OtpMailer

--- backend/app/mailers/otp_mailer.rb
+++ backend/app/mailers/otp_mailer.rb
@@ -1,11 +1,11 @@
 class OtpMailer < ApplicationMailer
   default from: "abdulkasif.ra@gmail.com"
 
-  def send_otp(email, otp_code)
-    @otp_code = otp_code
-    @email = email
+  def send_otp(otp_data)
+    @otp_code = otp_data.code
+    @email = otp_data.email
     mail(
-      to: email,
+      to: otp_data.email,
       subject: "Your OTP Code"
     )
   end
---
+++ backend/test/jobs/send_otp_mailer_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SendOtpMailerJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
