b2aa0a7f17859e5f24acce69eefb0ca8b0af27d8
abdul-kasif <abdulkasif.ra@gmail.com>
2025-09-14 02:07:43 +0530

    feat(dashboard-api): add DashboardController with show action and error handling

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   post "auth/send_otp", to: "auth#send_otp" # AuthController -> send_otp method
   post "auth/verify_otp", to: "auth#verify_otp" # AuthController -> verify_otp method
 
-  #Profile Creation routes
+  # Profile Creation routes
   post "/profile", to: "profile#create"
+
+  # Dashboard routes
+  get "/dashboard", to: "dashboard#show"
 end 
