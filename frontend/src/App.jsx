0be8b896ea0afd109389b58d2e0b87a77aee9591
nantha222 <krishnanantha02@gmail.com>
2025-09-13 19:19:02 +0530

    feat(setup): add initial component structure with Button and utility functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline text-center">
         Hello world!
       </h1>
+      <Button className="m-4" >Button</Button>
     </div>
   )
 }
