5a2738b15bcb75ed702ecd102631631d9712f767
abdul-kasif <abdulkasif.ra@gmail.com>
2025-09-14 14:35:44 +0530

    feat(dashboard-api): add protected dashboard endpoint returning user info, active and history sessions

--- backend/app/controllers/dashboard_controller.rb
+++ backend/app/controllers/dashboard_controller.rb
@@ -12,11 +12,24 @@ class DashboardController < ApplicationController
         emall: staff.email,
         staff_personal_id: staff.staff_personal_id,
         department: staff.department
-      }
+      },
+      active_sessions: active_sessions.as_json(only: [:id, :title, :google_form_url, :test_duration_minutes, :access_code, :started_at]),
+      history_sessions: history_sessions.as_json(only: [:id, :title, :google_form_url, :test_duration_minutes, :started_at, :ended_at])
     }, status: :ok
-
   rescue StandardError => e
     Rails.logger.error "Failed to fetch user info #{e.message}"
     return render json: { error: "Failed to fetch user info #{e.message}"}, status: :unprocessable_content
+  end
+
+  private
+
+  # Fetch all active assessment sessions for specified staff
+  def active_sessions
+    current_staff.assessment_sessions.status_active.order(started_at: :desc)
+  end
+  
+  # Fetch all completed asssessment sessions for specified staff
+  def history_sessions
+    current_staff.assessment_sessions.status_completed.order(started_at: :desc)
   end
 end
